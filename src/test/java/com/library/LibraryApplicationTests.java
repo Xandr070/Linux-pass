8e91c32812fe699d9bd86a274a0efe3f69e04dd2
Alexander Ivanov <alexandrivanov7070@gmail.com>
2025-05-24 21:38:58 +0300

    fix: test

--- src/test/java/com/library/LibraryApplicationTests.java
+++ src/test/java/com/library/LibraryApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.linux;
+package com.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LinuxApplicationTests {
+class LibraryApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-}
+} 
